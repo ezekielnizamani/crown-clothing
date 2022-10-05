b335a324368258037fc7f0cc9c0c43ca4b7d6470
ezekiel <ezekielnizamani0@gmail.com>
2022-10-05 00:54:27 -0700

    convertd product card scss to styled components

--- src/components/products-card/ProductCard.jsx
+++ src/components/products-card/ProductCard.jsx
@@ -1,23 +1,23 @@
 import { useContext} from "react";
 import Button from "../button/Button";
 import { CartContext } from "../../context/CartContext";
-import "./ProductCard.scss";
+import  {ProductCardContainer,Image,Footer,NameStyle,PriceStyle} from "./ProductCard.styles.jsx";
 const ProductCard = ({ product }) => {
   const { imageUrl, name, price } = product;
 
   const { addItemToCart} = useContext(CartContext);
  
   return (
-    <div className="product-card-container">
-      <img src={imageUrl} alt={`${name}`} />
-      <div className="footer">
-        <span className="name">{name}</span>
-        <span className="price">{price}</span>
-      </div>
+    <ProductCardContainer>
+      <Image src={imageUrl} alt={`${name}`} />
+      <Footer >
+        <NameStyle className="name">{name}</NameStyle>
+        <PriceStyle className="price">{price}</PriceStyle>
+      </Footer>
       <Button buttonType="inverted" click={() => addItemToCart(product)}>
         Add to cart
       </Button>
-    </div>
+    </ProductCardContainer>
   );
 };
 
