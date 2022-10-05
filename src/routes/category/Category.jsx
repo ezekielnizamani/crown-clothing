91a4d0b335a21fe2b433d3352980517c301abbfc
ezekiel <ezekielnizamani0@gmail.com>
2022-10-05 00:58:53 -0700

    coverted category scss to styled componets

--- src/routes/category/Category.jsx
+++ src/routes/category/Category.jsx
@@ -1,27 +1,28 @@
-import {useContext,useEffect,useState} from 'react'
-import { useParams } from 'react-router-dom'
-import { CategoriesContext } from '../../context/categoriesContext'
-import ProductCard from '../../components/products-card/ProductCard'
-import "./Category.scss"
+import { useContext, useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { CategoriesContext } from "../../context/categoriesContext";
+import ProductCard from "../../components/products-card/ProductCard";
+import {CategoryPageContainer,CategoryTitle} from "./Category.styles";
 const Category = () => {
-    const {category} = useParams();
-    const {categoriesMap} = useContext(CategoriesContext)
-    const [products,setProducts] = useState(categoriesMap[category]);
+  const { category } = useParams();
+  const { categoriesMap } = useContext(CategoriesContext);
+  const [products, setProducts] = useState(categoriesMap[category]);
 
-    useEffect(()=>{
-        setProducts(categoriesMap[category])
-    },[category,categoriesMap])
+  useEffect(() => {
+    setProducts(categoriesMap[category]);
+  }, [category, categoriesMap]);
 
   return (
     <>
-    <h2 className='category-title'>{category.toUpperCase()}</h2>
-    <div className='category-page-container'>
-    {products && products.map(product => (
-        <ProductCard key={product.id} product={product}/>
-    ))}
-    </div>
-</>
-  )
-}
+      <CategoryTitle>{category.toUpperCase()}</CategoryTitle>
+      <CategoryPageContainer className="category-page-container">
+        {products &&
+          products.map((product) => (
+            <ProductCard key={product.id} product={product} />
+          ))}
+      </CategoryPageContainer>
+    </>
+  );
+};
 
-export default Category
+export default Category;
---
+++ src/routes/category/Category.styles.jsx
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+export const CategoryTitle = styled.h2`
+  font-size: 38px;
+  text-align: center;
+  margin-bottom: 40px;
+`;
+export const CategoryPageContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-column-gap: 20px;
+  grid-row-gap: 50px;
+`;
+
+// .category-title{
+//     font-size: 38px;
+//     text-align: center;
+//     margin-bottom: 40px;
+// }
+// .category-page-container {
+//   display: grid;
+//   grid-template-columns: repeat(4, 1fr);
+//   grid-column-gap: 20px;
+//   grid-row-gap: 50px;
+// }
