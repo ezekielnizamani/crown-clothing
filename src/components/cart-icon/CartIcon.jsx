0a73490813553b7e2426e003463da8146a3e094c
ezekiel <ezekielnizamani0@gmail.com>
2022-10-05 05:11:33 -0700

    used svg icon in styled component cartIcon.styles.jsx insted of in carticon component cartIcon.jsx

--- src/components/cart-icon/CartIcon.jsx
+++ src/components/cart-icon/CartIcon.jsx
@@ -1,22 +1,15 @@
 import { useContext } from "react";
-import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
 import { CartContext } from "../../context/CartContext";
-import {
-  CartIconContainer,
-  ItemCount,
-  ShoppingIconStyle,
-} from "./CartIcon.syles";
+import {CartIconContainer,ItemCount,ShoppingIcon} from "./CartIcon.syles";
 const CartIcon = () => {
-  const { isCartOpen, setIsCartOpen, cartCount } = useContext(CartContext);
+  const { isCartOpen, setIsCartOpen ,cartCount} = useContext(CartContext);
   const onClickHandler = () => {
     setIsCartOpen(!isCartOpen);
   };
   return (
-    <CartIconContainer onClick={onClickHandler}>
-      <ShoppingIconStyle>
-        <ShoppingIcon />
-      </ShoppingIconStyle>
-      <ItemCount>{cartCount}</ItemCount>
+    <CartIconContainer  onClick={onClickHandler}>
+      <ShoppingIcon/>
+      <ItemCount >{cartCount}</ItemCount>
     </CartIconContainer>
   );
 };
